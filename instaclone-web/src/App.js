88b91a7509b4d12f4341c8e47234c5d019678031
menuin <menuin@hanyang.ac.kr>
2021-07-20 04:31:18 +0900

    #8.9 GlobalStyles

--- instaclone-web/src/App.js
+++ instaclone-web/src/App.js
@@ -6,21 +6,16 @@ import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
-const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "lightgray"
-};
-const darkTheme = {
-  fontColor: "lightgray",
-  bgColor: "#2c2c2c",
-};
+
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar)
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
       <Router>
         <Switch>
           <Route path="/" exact>
--- instaclone-web/src/screens/Login.js
+++ instaclone-web/src/screens/Login.js
@@ -6,7 +6,6 @@ const Title = styled.h1`
     color : ${(props) => props.theme.fontColor};
 `
 const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
 `
 
 
