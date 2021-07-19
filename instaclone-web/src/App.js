f2a9205fcbd976ea8d8096ee20e8059c537a2e7e
menuin <menuin@hanyang.ac.kr>
2021-07-20 02:55:51 +0900

    #8.6 react variables

---
+++ instaclone-web/src/App.js
@@ -0,0 +1,25 @@
+import { useReactiveVar } from "@apollo/client";
+import { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { isLoggedInVar } from "./apollo";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
+function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
