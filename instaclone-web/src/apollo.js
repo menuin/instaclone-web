35119e3496727fa349a8e4406e9daed8270e0786
menuin <menuin@hanyang.ac.kr>
2021-07-24 17:42:12 +0900

    #10.14 dark mode

--- instaclone-web/src/apollo.js
+++ instaclone-web/src/apollo.js
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-const TOKEN = "token";
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE"
 export const isLoggedInVar = makeVar(localStorage.getItem(TOKEN));
 export const logUserIn = (token) => {
     localStorage.setItem(TOKEN, token);
@@ -12,8 +13,16 @@ export const logUserOut = (token) => {
     window.location.reload();
 };
 
+export const enableDarkMode = () => {
+    localStorage.setItem(DARK_MODE, "enabled");
+    darkModeVar(true);
+}
 
-export const darkModeVar = makeVar(false);
+export const disableDarkMode = () => {
+    localStorage.setItem(DARK_MODE, "disabled");
+    darkModeVar(false);
+}
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
 export const client = new ApolloClient({
     uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
