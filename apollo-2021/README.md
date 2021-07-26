6ae3c324367441a02e9fe11c1e0754fe1ed8b080
menuin <menuin@hanyang.ac.kr>
2021-07-27 03:15:09 +0900

    update

--- apollo-2021/README.md
+++
@@ -1,3 +0,0 @@
-# Apollo 2021
-
-Movie app built with React, Apollo and GraphQL
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import { useReactiveVar } from "@apollo/client";
 import { faInstagram, faPhabricator } from "@fortawesome/free-brands-svg-icons";
 import { faCompass, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { isLoggedInVar } from "../apollo";
+import useUser from "../hooks/useUser";
+import routes from "../routes";
 
 const SHeader = styled.header`
     width:100%;
@@ -27,8 +30,16 @@ const Column = styled.div``;
 const Icon = styled.span`
     margin-left : 15px;
 `
+const Button = styled.span`
+    background-color : ${props => props.theme.accent};
+    border-radius : 4px;
+    padding : 5px 15px;
+    color : white;
+    font-weight : 600;
+`
 function Header() {
     const isLoggedIn = useReactiveVar(isLoggedInVar);
+    const loggedInUser = useUser();
     return (
         <SHeader>
             <Wrapper>
@@ -36,15 +47,21 @@ function Header() {
                     <FontAwesomeIcon icon={faInstagram} size="2x" />
                 </Column>
                 <Column>
-                    <Icon>
-                        <FontAwesomeIcon icon={faHome} size="lg" />
-                    </Icon>
-                    <Icon>
-                        <FontAwesomeIcon icon={faCompass} size="lg" />
-                    </Icon>
-                    <Icon>
-                        <FontAwesomeIcon icon={faUser} size="lg" />
-                    </Icon>
+                    {isLoggedIn ? (<>
+                        <Icon>
+                            <FontAwesomeIcon icon={faHome} size="lg" />
+                        </Icon>
+                        <Icon>
+                            <FontAwesomeIcon icon={faCompass} size="lg" />
+                        </Icon>
+                        <Icon>
+                            <FontAwesomeIcon icon={faUser} size="lg" />
+                        </Icon></>
+                    ) : (
+                        <Link href={routes.home}>
+                            <Button>Login</Button>
+                        </Link>
+                    )}
                 </Column>
             </Wrapper>
         </SHeader>
