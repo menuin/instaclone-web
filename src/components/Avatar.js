b1882b527902270a2cf229c813bb72b7f44eb54f
menuin <menuin@hanyang.ac.kr>
2021-07-28 02:58:43 +0900

    ~#11.5 photo component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 
 const SAvatar = styled.div`
-    width : 18px;
-    height : 18px;
-    border-radius : 15px;
+    width : ${props => props.lg ? "30px" : "25px"};
+    height : ${props => props.lg ? "30px" : "25px"};
+    border-radius : 50%;
     background-color : #2c2c2c;
     overflow : hidden;
 `
 const Img = styled.img`
     max-width: 100%;
 `
-function Avatar({ url = "" }) {
-    return (<SAvatar>
-        {url !== "" ? <Img src={url} /> : null}
-    </SAvatar>)
+function Avatar({ url = "", lg = false }) {
+    return (
+        <SAvatar lg={lg}>
+            {url !== "" ? <Img src={url} /> : null}
+        </SAvatar>)
 }
 
 export default Avatar;
